7779ae13514e43f003894316a244851182e560e6
SidorkinAlex <palach.orl@mail.ru>
2021-10-28 00:46:12 +0300

    added lib to serialize Closure and started creating job class

--- src/job/Job.php
+++ src/job/Job.php
@@ -3,5 +3,27 @@
 namespace SidorkinAlex\PhpCoroutine\Job;
 class Job implements JobInterface
 {
+    protected string $id;
+    protected bool $olyOneWorker = false;
+    protected string $queueName = "IO.queue";
+    protected int $workerLimit = 100;
+    protected $function;
+    protected string $serializedFunction;
+    protected array $functionParams;
+
+    /**
+     * Job constructor.
+     * @param array $functionParams
+     * @param callable $function
+     * @param string $queueName
+     */
+    public function __construct(array $functionParams, callable $function, string $queueName = "")
+    {
+        \Opis\Closure\Library::init();
+        $this->function = $function;
+        $this->serializedFunction = serialize($function);
+        $this->functionParams = $functionParams;
+    }
+
 
 }
--- src/job/JobInterface.php
+++ src/job/JobInterface.php
@@ -6,5 +6,6 @@ namespace SidorkinAlex\PhpCoroutine\job;
 
 interface JobInterface
 {
+    public function __construct(array $functionParams, callable $function, string $queueName="");
 
 }
